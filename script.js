4397bd0d31ba23ddf3e0c16544e6baedf629ccd8
avi-codesmith <avi.codesmith@gmail.com>
2025-04-26 20:05:05 +0530

    Add Changes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const hover = document.querySelector(".hover");
+const imgSec = document.querySelector(".product");
+const needHover = document.querySelector(".needHover");
+const circle = document.querySelector(".circle");
+const products = document.querySelector(".img-sec");
+
 const scroll = new LocomotiveScroll({
   el: document.querySelector("main"),
   smooth: true,
@@ -29,11 +35,52 @@ const loadingAnimation = () => {
 
 loadingAnimation();
 
-const imgSec = document.querySelector(".product-wrappper");
-
 const obs = new IntersectionObserver((enteries) => {
   const ent = enteries[0];
-  console.log(ent);
+  if (ent.isIntersecting === true) {
+    gsap.from(".product", {
+      scale: 0.99,
+      opacity: 0,
+      delay: 0.4,
+      duration: 0.5,
+    });
+  } else {
+    gsap.from(".product", {
+      scale: 1,
+      opacity: 1,
+    });
+  }
 });
 
 obs.observe(imgSec);
+
+const hoverAnimation = () => {
+  needHover.addEventListener("mouseenter", () => {
+    hover.classList.add("move");
+  });
+  needHover.addEventListener("mouseleave", () => {
+    hover.classList.remove("move");
+  });
+};
+
+hoverAnimation();
+
+const cursorMove = () => {
+  products.addEventListener("mouseenter", (e) => {
+    gsap.to(".circle", {
+      opacity: 1,
+      scale: 1,
+      x: e.clientX,
+      y: e.clientY,
+    });
+  });
+
+  products.addEventListener("mouseleave", () => {
+    gsap.to(".circle", {
+      opacity: 0,
+      scale: 0,
+    });
+  });
+};
+
+cursorMove();
